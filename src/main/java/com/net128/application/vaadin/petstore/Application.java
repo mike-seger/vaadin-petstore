630b5f56eda06a07f2cdf8875bccb02eac9f7308
Michael Seger <michael@macbookpro>
2020-06-14 14:08:40 +0200

    updated deps

--- src/main/java/com/net128/application/vaadin/petstore/Application.java
+++ src/main/java/com/net128/application/vaadin/petstore/Application.java
@@ -2,9 +2,10 @@ package com.net128.application.vaadin.petstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
