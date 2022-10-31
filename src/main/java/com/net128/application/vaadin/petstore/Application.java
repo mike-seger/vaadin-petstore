62205905e08af945d8466888a2bcbb393145e16e
michael <michael@mac>
2022-10-31 20:33:21 +0100

    ...

--- src/main/java/com/net128/application/vaadin/petstore/Application.java
+++ src/main/java/com/net128/application/vaadin/petstore/Application.java
@@ -4,7 +4,6 @@ import com.logviewer.springboot.LogViewerSpringBootConfig;
 import com.net128.oss.web.lib.filemanager.FileManagerServlet;
 import com.net128.oss.web.lib.jpa.csv.JpaCsv;
 import com.net128.oss.web.webshell.WebShell;
-//import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -18,6 +17,13 @@ import org.springframework.context.annotation.Import;
 })
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
+        String tmpDir = System.getProperty("java.io.tmpdir");
+        if(tmpDir!=null) {
+            if(!tmpDir.endsWith("/") && !tmpDir.endsWith("\\")) {
+                System.setProperty("java.io.tmpdir", tmpDir+"/");
+            }
+        }
+
         SpringApplication.run(Application.class);
     }
 }
